6a2e51d62d58f4089975961a8186d3d9de53355f
Prashant Yelpale <144728514+PrashantYelpale@users.noreply.github.com>
2025-04-02 14:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
  
-app.use(express.static('public'));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
  
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
