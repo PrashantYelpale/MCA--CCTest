f0149af5288aa10cf68e1739f5b6d1f18ac3dbfb
Prashant Yelpale <144728514+PrashantYelpale@users.noreply.github.com>
2025-04-02 15:26:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
- 
-// Serve blocksdk.js from the root directory
-app.get('/node_modules/blocksdk/blocksdk.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'blocksdk.js'));
-});
- 
-// Serve script.js from the root directory
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'script.js'));
-});
- 
+
+// Serve static files from the project directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve node_modules files
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
- 
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
